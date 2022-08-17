edb3e7e0ead737b06ccb39d2ec53af3b4e7e8d2b
augustobor <augus_borromeo@hotmail.com>
2022-08-17 02:58:13 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view/>
+</template>
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;  
+}
+
+#app {
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Nuevo proyecto 'Soy-Zar'</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
+
+<style>
+h1 {
+  color: #FAFAFA;
+}
+</style>
